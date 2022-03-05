288157746f3870736aa94ece8b7fa89e25b0baf6
PandBonf <pandbon@gmail.com>
2022-03-05 20:01:03 +0300

    test1

---
+++ Test1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Test1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				FailProcess();
+			}
+			catch {
+				Console.WriteLine("поймана ошибка");
+				Console.ReadKey();
+			}
+
+			Console.WriteLine("Failed to fail process!");
+			Console.ReadKey();
+		}
+
+		static void FailProcess()
+		{
+			throw new Exception();
+		}
+
+		static void FailProcess2()
+        {
+            Process process = Process.GetCurrentProcess();
+			process.Kill();
+		}
+	}
+}
+
